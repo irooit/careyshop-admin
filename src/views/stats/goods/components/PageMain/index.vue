964a594d1d3b38b25ae912d8a9718aa79b0cad63
思维空间 <dnyz520@126.com>
2020-09-24 02:19:31 +0800

    no message

--- src/views/stats/goods/components/PageMain/index.vue
+++ src/views/stats/goods/components/PageMain/index.vue
@@ -20,13 +20,32 @@
       </el-col>
 
       <el-col :span="10">
-        <el-card class="box-card" shadow="never">
-          <div slot="header" class="box-card-header">
-            <span>热销 TOP</span>
-          </div>
+        <el-table
+          :data="topData"
+          :header-cell-style="{padding: '9px 0'}"
+          class="table-card">
+          <el-table-column
+            type="index"
+            label="排行"
+            width="50">
+          </el-table-column>
 
-          <div>111</div>
-        </el-card>
+          <el-table-column
+            label="名称"
+            show-overflow-tooltip>
+            <template slot-scope="scope">
+              <span
+                @click="handleView(scope.row.goods_id)"
+                class="link">{{scope.row.name}}</span>
+            </template>
+          </el-table-column>
+
+          <el-table-column
+            prop="sales_sum"
+            label="销售量"
+            width="100">
+          </el-table-column>
+        </el-table>
       </el-col>
     </el-row>
   </div>
@@ -62,18 +81,38 @@ export default {
     }
   },
   methods: {
+    // 打开商品预览
+    handleView(goods_id) {
+      this.$router.push({
+        name: 'goods-admin-view',
+        params: { goods_id }
+      })
+    }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   .box-card {
     border-radius: 0;
-    border: 1px solid #DCDFE6;
+    border: 1px solid $color-border-1;
   }
 
   .box-card-header {
     font-size: 14px;
-    color: #606266;
+    color: $color-text-normal;
+  }
+
+  .table-card {
+    border: 1px solid $color-border-1;
+    border-bottom: none;
+  }
+
+  .link {
+    &:hover {
+      cursor: pointer;
+      color: $color-primary;
+      text-decoration: underline;
+    }
   }
 </style>
