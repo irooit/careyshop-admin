7862d41d8a6359719e3f357c31134883eca5547d
思维空间 <dnyz520@126.com>
2020-09-23 09:49:30 +0800

    路由、页面进行准备

--- src/router/modules/stats.js
+++ src/router/modules/stats.js
@@ -11,6 +11,30 @@ export default {
       name: `${pre}index`,
       meta: { ...meta, cache: true, title: '数据首页' },
       component: () => import('@/views/stats/index')
+    },
+    {
+      path: 'data/shop',
+      name: `${pre}data-shop`,
+      meta: { ...meta, cache: true, title: '店铺统计' },
+      component: () => import('@/views/stats/shop')
+    },
+    {
+      path: 'data/goods',
+      name: `${pre}data-goods`,
+      meta: { ...meta, cache: true, title: '商品统计' },
+      component: () => import('@/views/stats/goods')
+    },
+    {
+      path: 'data/order',
+      name: `${pre}data-order`,
+      meta: { ...meta, cache: true, title: '订单统计' },
+      component: () => import('@/views/stats/order')
+    },
+    {
+      path: 'data/client',
+      name: `${pre}data-client`,
+      meta: { ...meta, cache: true, title: '会员统计' },
+      component: () => import('@/views/stats/client')
     }
   ])('stats-')
 }
---
+++ src/views/stats/goods/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'stats-data-goods'
+}
+</script>
+
+<style scoped>
+</style>
