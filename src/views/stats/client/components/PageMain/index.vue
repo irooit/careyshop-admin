41d8ef5c127dc5d9f2b7d19ac572342b530de762
思维空间 <dnyz520@126.com>
2020-09-24 10:45:31 +0800

    no message

--- src/views/stats/client/components/PageMain/index.vue
+++ src/views/stats/client/components/PageMain/index.vue
@@ -1,13 +1,84 @@
 <template>
+  <div class="cs-p">
+    <el-card class="box-card" shadow="never">
+      <div slot="header" class="box-card-header">
+        <span>今日实时</span>
+      </div>
 
+      <div class="cs-today flex-wrap">
+        <div
+          v-for="(item, key) in todayData"
+          :key="key"
+          class="cs-today__block">
+          <div class="cs-today__content">
+            <div class="cs-today__info">
+              <p>{{todayMap[key]}}</p>
+              <p class="cs-today__number">{{item}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-name: "index"
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.box-card {
+  border-radius: 0;
+  border: 1px solid $color-border-1;
+}
 
+.box-card-header {
+  font-size: 14px;
+  color: $color-text-normal;
+}
+
+.table-card {
+  border: 1px solid $color-border-1;
+  border-bottom: none;
+}
+
+.flex-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  margin: -10px;
+}
+
+.cs-today {
+  .cs-today__block {
+    width: 20%;
+    box-sizing: border-box;
+  }
+
+  .cs-today__content {
+    display: flex;
+    color: $color-info;
+    border-radius: 4px;
+    background-color: #F5F7FA;
+    margin: 10px;
+    overflow: hidden;
+  }
+
+  .cs-today__info {
+    width: 100%;
+    text-align: center;
+
+    p {
+      margin: 10px;
+      font-size: 14px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    .cs-today__number {
+      color: $color-text-normal;
+      font-size: 28px;
+    }
+  }
+}
 </style>
