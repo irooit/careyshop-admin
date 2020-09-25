be34fb3222a04f3cfcab9183ee6de3a4b2e7f347
思维空间 <dnyz520@126.com>
2020-09-25 17:25:57 +0800

    后台首页布局内容重构

--- src/views/index/components/PageMain/index.vue
+++ src/views/index/components/PageMain/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div style="height: 1000px"></div>
+  <div class="cs-p">
+    <div class="cs-card">
+      <span>111</span>
+      <div>
+        <span>222</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,5 +14,12 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.cs-card {
+  border: 1px solid #DDDEE1;
+  background: #FFF;
+  font-size: 14px;
+  position: relative;
+  padding: 20px;
+}
 </style>
